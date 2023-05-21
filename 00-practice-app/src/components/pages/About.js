99866497f929205877b1f922de4268812ed94a28
bcebel <bcebel@gmail.com>
2023-05-21 18:43:35 -0400

    customizing

---
+++ 00-practice-app/src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import bgImage from './spiral.png'
+export default function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        <img src={bgImage} />I am a highly skilled and adaptable Full Stack Web
+        Developer with a strong background in environmental and data science. My
+        experience includes collaborating with prestigious colleges and
+        universities nationwide, managing state and national reporting for
+        polluted properties, and leveraging my expertise to bridge the gap
+        between the real world and the virtual world.
+      </p>
+    </div>
+  );
+}
---
+++ 00-practice-app/src/components/pages/Blog.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function Blog() {
+  return (
+    <div>
+      <h1>Portfolio</h1>
+      <p>
+        <a
+          href="https://bcebel.github.io/WebAPICodeQuiz/"
+          rel="noopener"
+          target="_blank"
+        >
+          Web API Code Quiz
+        </a>
+        <a
+          href="https://phudireservations.herokuapp.com/"
+          rel="noopener"
+          target="_blank"
+        >
+          Reservation System
+        </a>
+        <a
+          href="https://bcebel.github.io/PasswordGenerator/"
+          rel="noopener"
+          target="_blank"
+        >
+          Password Generator
+        </a>
+        <a
+          href="https://bcebel.github.io/Hot10/"
+          rel="noopener"
+          target="_blank"
+        >
+          Music Top 10
+        </a>
+        put these in as react
+      </p>
+    </div>
+  );
+}
