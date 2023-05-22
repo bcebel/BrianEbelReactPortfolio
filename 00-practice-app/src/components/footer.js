b6eba7223eec24d9c1494906855a2f953b3d42f4
bcebel <bcebel@gmail.com>
2023-05-21 21:47:31 -0400

    added footer

---
+++ 00-practice-app/src/components/footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// TODO: Add a comment explaining how we are able to extract the key value pairs from props
+
+function footer() {
+  return <footer>wut</footer>;
+}
+
+export default footer;
--- 00-practice-app/src/components/pages/About.js
+++ 00-practice-app/src/components/pages/About.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import bgImage from './spiral.png'
+import React from "react";
 export default function About() {
   return (
     <div>
-      <h1>About Me</h1>
+      <h1>About Page</h1>
       <p>
-        <img src={bgImage} />I am a highly skilled and adaptable Full Stack Web
-        Developer with a strong background in environmental and data science. My
-        experience includes collaborating with prestigious colleges and
-        universities nationwide, managing state and national reporting for
-        polluted properties, and leveraging my expertise to bridge the gap
-        between the real world and the virtual world.
+        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
+        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
+        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
+        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
+        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
+        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
       </p>
     </div>
   );
