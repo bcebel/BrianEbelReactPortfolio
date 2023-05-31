5488d1339a69841b43b256917410e844f97e9d26
bcebel <bcebel@gmail.com>
2023-05-30 20:00:54 -0400

    added github links

--- react-app/src/components/pages/Blog.js
+++ react-app/src/components/pages/Blog.js
@@ -12,23 +12,60 @@ export default function Blog() {
               href="https://phudireservations.herokuapp.com/"
               target="_blank"
             ></a>
+            <a href="https://github.com/ChrisJCota/phudi" target="_blank">
+              PHÜDI Github
+            </a>
             <a
               href="https://bcebel.github.io/WebAPICodeQuiz/"
               target="_blank"
             ></a>
-            <a href="https://stormy-springs-91791.herokuapp.com/"></a>
+            <a href="https://github.com/bcebel/WebAPICodeQuiz/" target="_blank">
+              Quiz Wars Github
+            </a>
+
+            <a
+              href="https://stormy-springs-91791.herokuapp.com/"
+              target="_blank"
+            ></a>
+            <a href="https://github.com/bcebel/PWA" target="_blank">
+              Progressive Web App Github
+            </a>
+
             <a
               href="https://sleepy-caverns-33323.herokuapp.com/"
               target="_blank"
             ></a>
+            <a href="https://github.com/bcebel/techblog" target="_blank">
+              Tech Blog Github
+            </a>
+
             <a
               href="https://bcebel.github.io/PasswordGenerator/"
               target="_blank"
             ></a>
-            <a href="https://bcebel.github.io/Hot10/" target="_blank"></a>
-            <a href="https://bcebel.github.io/met/" target="_blank"></a>
-            <a href="https://bcebel.github.io/BrianPortfolio/" target="_blank">
+            <a
+              href="https://github.com/bcebel/PasswordGenerator/"
+              target="_blank"
+            >
+              Password Generator Github
+            </a>
 
+            <a href="https://bcebel.github.io/Hot10/" target="_blank"></a>
+            <a href="https://github.com/bcebel/Hot10/" target="_blank">
+              Hot 10 Github
+            </a>
+
+            <a href="https://bcebel.github.io/met/" target="_blank"></a>
+            <a href="https://github.com/bcebel/met" target="_blank">
+              Weather App Github
+            </a>
+
+            <a
+              href="https://bcebel.github.io/BrianPortfolio/"
+              target="_blank"
+            ></a>
+            <a href="https://github.com/bcebel/BrianPortfolio/" target="_blank">
+              Brian CSS Portfolio Github
             </a>
           </section>
         </div>
